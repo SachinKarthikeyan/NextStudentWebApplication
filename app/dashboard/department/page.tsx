38def9f1d02fd964c050f6771e297c338c9ce908
SachinKarthikeyan <sachinkarthikeyan3303@gmail.com>
2023-11-06 15:59:45 +0530

    Commit

--- app/dashboard/department/page.tsx
+++ app/dashboard/department/page.tsx
@@ -4,7 +4,7 @@ import userData from "../../../admin/user-data.json"
 
 import GetResultForm from '@/app/ui/dashboard/Form';
 
-export default function Department() {
+export default function Layout({ children }: { children: React.ReactNode }) {
   const userRole = userData[0].role; 
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
@@ -13,6 +13,7 @@ export default function Department() {
       </div>
       
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+
         <div className="mt-4 flex items-center justify-between gap-2 md:mt-0">
           <Search placeholder="Search" />
         </div>
